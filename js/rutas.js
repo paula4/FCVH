8d2b52e882678ee81cdf1fb89a1b5109e3dfab94
paula4 <pauliperez_4@hotmail.com>
2017-10-27 14:25:22 -0300

    stiilo

--- js/rutas.js
+++ js/rutas.js
@@ -1,27 +1,54 @@
+var menu =[
+  {
+    title:'Inicio',
+    url:'template/inicio.html',
+    route:'/',
+    inmenu:true
+  },
+  {
+    title:'Historia',
+    url:'template/historia.html',
+    route:'/historia',
+    inmenu:true
+  },
+  {
+    title:'Pasión',
+    url:'template/pasion.html',
+    route:'/pasion',
+    inmenu:true
+  },
+  {
+    title:'Contacto',
+    url:'template/contacto.html',
+    route:'/contacto',
+    inmenu:true
+  },
+  {
+    title:'Tienda',
+    url:'template/shop.html',
+    route:'/shop',
+    inmenu:true
+  },
+  {
+    title:'404',
+    url:'template/404.html',
+    route:'/404',
+    inmenu:false
+  }];
 var villa = angular.module('fcvhapp', ['ngRoute',]);
 villa.config(['$routeProvider' ,'$locationProvider',
 function ($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('');
-  $routeProvider.
-  when('/', {
-    templateUrl: 'template/inicio.html',
-  }).
-  when('/historia', {
-    templateUrl: 'template/historia.html',
-  }).
-  when('/pasion', {
-    templateUrl: 'template/pasion.html',
-  }).
-  when('/contacto', {
-    templateUrl: 'template/contacto.html',
-  }).
-  when('/shop', {
-    templateUrl: 'template/shop.html',
-  }).
-  when('/404', {
-    templateUrl: 'template/404.html',
-  }).
-  otherwise({
+  menu.forEach(function (item){
+    $routeProvider.when(item.route, {
+      templateUrl: item.url
+    });
+  });
+  $routeProvider.otherwise({
     redirectTo: '/404',
   });
+}]);
+
+villa.controller('MenuCtrl', ['$scope', function($scope) {
+  $scope.items = menu;
 }]);
